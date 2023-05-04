b52e775a890deb287eb235d80eb70c949e9d3ddf
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-04 17:33:51 +0300

    useless declaration removed

--- Engine/src/utils/prompt.h
+++ Engine/src/utils/prompt.h
@@ -14,5 +14,3 @@ string* prompt(char prompt_char, string message, string *accepted_commands);
 void print(string message);
 void print(int message);
 void print(char message);
-
-void runCommand(string command, string arg, Item **item);
