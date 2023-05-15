5e859d911fc296f421fbf7f555142268d074a0cc
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-15 17:02:08 +0300

    moveToNode additions

--- Engine/src/player/player.cpp
+++ Engine/src/player/player.cpp
@@ -41,19 +41,27 @@ bool exists(vector<Node*> vector, Node *node) {
  * @param player The player object
  */
 void Player::moveToNode(Node *newNode) {
-    if(!exists(this->currentNode->accessible_nodes, newNode)) return; // if the new node is not accessible from the current node return (to avoid jumps)
+    if(!currentNode->isAccessible()){
+        println("The room you are in is currently locked!");
+        return;
+    }
+
+    if(!exists(this->currentNode->accessible_nodes, newNode)) { // if the new node is not accessible from the current node return (to avoid jumps)
+        println("The room you want to go is not accessible from your current one");
+        return;
+    }
     /**
      * @brief If player tries to go to an inaccessible node, it will print the newNode description.
      */
     if (!newNode->isAccessible()) {
-        print(newNode -> description);
+        println(newNode->description + " is locked");
     }
     /**
      * @brief If the player tries to enter an accessible node, it changes the currentNode parameter inside the player object to the new one. Finally it prints the new node  
      */
     else {
         this->currentNode = newNode;
-        print(newNode -> description);
+        println(newNode -> description);
     }
 
 }
